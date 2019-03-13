65dbef586faa8c68bc96c995a97b9d1a73d83d88
HarshaKy <harshaky6573@gmail.com>
2019-03-13 20:57:46 +0530

    more setup

--- src/app.js
+++ src/app.js
@@ -2,9 +2,16 @@ const path = require('path')
 const express = require('express')
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+// Define paths for express
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates')
+
+// setup handlebars engine and views location
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
+// setup directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
