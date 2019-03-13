7e8fb08e9d398d0c0d9f7aeb9a6cfab9c1a5f4cc
HarshaKy <harshaky6573@gmail.com>
2019-03-13 19:48:09 +0530

    adsf

--- src/app.js
+++ src/app.js
@@ -6,17 +6,6 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-app.get('/help', (req, res) => {
-  res.send({
-    name: 'Asdf',
-    age: 21
-  })
-})
-
-app.get('/about', (req, res) => {
-  res.send("<h1>About</h1>")
-})
-
 app.get('/weather', (req, res) => {
   res.send({
     location: "Bangalore",
