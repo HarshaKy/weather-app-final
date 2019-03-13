daaff47d9b244b1c39b3404814e42858fda0dfb1
HarshaKy <harshaky6573@gmail.com>
2019-03-13 20:42:43 +0530

    setting up templates

--- src/app.js
+++ src/app.js
@@ -8,7 +8,23 @@ app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
-  res.render('index')
+  res.render('index', {
+    title: 'Weather',
+    name: 'Harsha'
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'Harsha'
+  })
+})
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    helpText: "Some Helpful Text"
+  })
 })
 
 app.get('/weather', (req, res) => {
