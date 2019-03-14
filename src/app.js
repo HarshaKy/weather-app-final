eb9bc4164eb8cee9f23fb81ff8a520888933cdb8
HarshaKy <harshaky6573@gmail.com>
2019-03-14 23:33:53 +0530

    setup 404 pages

--- src/app.js
+++ src/app.js
@@ -46,6 +46,22 @@ app.get('/weather', (req, res) => {
   })
 })
 
+app.get('/help/*', (req, res) => {
+	res.render('404', {
+		title: '404',
+		name: 'Harsha',
+		errorMessage: 'Help Article Not Found.'
+	})
+})
+
+app.get('*', (req, res) => {
+	res.render('404', {
+		title: '404',
+		name: 'Harsha',
+		errorMessage: 'Page Not Found.'
+	})
+})
+
 app.listen(3000, () => {
   console.log("Server is up on port 3000");
 })
