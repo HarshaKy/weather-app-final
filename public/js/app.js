42e64a21da31578dfe17a43bd35fad6836f935e0
HarshaKy <harshaky6573@gmail.com>
2019-03-23 00:38:14 +0530

    takes place name and gives weather info!

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,10 @@ console.log("Client side js file is loaded")
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+// messageOne.textContent = 'test'
 
 weatherForm.addEventListener('submit', (event) => {
 	event.preventDefault()
@@ -11,10 +15,10 @@ weatherForm.addEventListener('submit', (event) => {
 	fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
 		response.json().then((data) => {
 			if (data.error) {
-				console.log(data.error)
+				messageOne.textContent = data.error
 			} else {
-				console.log(data.location)
-				console.log(data.forecastData)
+				messageOne.textContent = data.location
+				messageTwo.textContent = data.forecastData
 			}
 		})
 	})
