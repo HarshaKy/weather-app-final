510b8f7d9361fbd41ad08902003c3e0c80b3391e
HarshaKy <harshaky6573@gmail.com>
2019-03-23 00:58:53 +0530

    basic styling

--- public/js/app.js
+++ public/js/app.js
@@ -5,12 +5,13 @@ const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
-// messageOne.textContent = 'test'
-
 weatherForm.addEventListener('submit', (event) => {
 	event.preventDefault()
 
 	const location = search.value
+
+	messageOne.textContent = 'Loading...'
+	messageTwo.textContent = ''
 
 	fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
 		response.json().then((data) => {
